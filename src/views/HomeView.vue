fafa6eca9c01537bd559ff59b1500ea42db35b93
Matej7418 <matejsl404@gmail.com>
2023-09-20 18:26:43 +0200

    temp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+    <SvgMap
+    :map="map"
+    view-box="0 0 25220 23850"
+    :thin-border-on-zoom="true"
+  />
   </main>
 </template>
+
+<script>
+import TheWelcome from '../components/TheWelcome.vue'
+import SvgMap from 'vue-simple-svg-map'
+import map from '@/assets/map.js'
+
+export default {
+  name: 'HomeView',
+  components: {TheWelcome, SvgMap},
+  data () {
+    return {
+      map
+    }
+  }
+}
+</script>
